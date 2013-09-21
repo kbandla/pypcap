ac61dac693dfe76937531931a0ab6146f8f532f5
Kiran Bandla <kbandla@in2void.com>
2013-09-21 10:16:28 -0400

    Started adding unittests

--- test.py
+++ test.py
@@ -1,32 +1,71 @@
-# pypcap test file
+# pypcap unittests
+import os
+import sys
+import glob
+import unittest
 
+sys.path.insert(0, glob.glob('build/lib.*')[0])
 import pypcap
-import dpkt
 
-# module functions
-print 'lookupdev = ', pypcap.pcap_lookupdev()
-print 'findalldevs = ', pypcap.pcap_findalldevs()
-print 'pcap_lookupnet(eth0) = ', pypcap.pcap_lookupnet('eth0')
+try:
+    import dpkt
+except Exception,e:
+    dpkt = False
 
-# create a pcap
-x = pypcap.pcap()
-print 'pcap_create = ', x.pcap_create('eth0')
-print 'interface = ', x.interface
-print 'pcap_set_buffer_size = ', x.pcap_set_buffer_size(1000)
-print 'pcap_set_snaplen = ', x.pcap_set_snaplen(576)
-print 'pcap_set_timeout = ', x.pcap_set_timeout(512)
-print 'pcap_set_promisc = ', x.pcap_set_promisc(0)
-print 'pcap_activate = ', x.pcap_activate()
-print 'pcap_compile = ', x.pcap_compile('udp port 53')
+class ModuleTests(unittest.TestCase):
+    '''
+    Various tests on the module-level functionality 
+    '''
+    def setUp(self):
+        pass
 
-def pcap_callback(pkt):
-    # do stuff with the packet
-    eth = dpkt.ethernet.Ethernet(pkt)
-    ip = eth.data
-    udp = ip.data
-    dns = dpkt.dns.DNS(udp.data)
-    for qd in dns.qd:
-        print qd.name
+    def test0_import(self):
+        try:
+            import pypcap
+        except Exception,e:
+            self.assertTrue(False, 'Could not import pypcap : %s'%(e))
 
-print 'pcap_set_callback = ', x.pcap_set_callback(pcap_callback)
-print 'pcap_loop = ', x.pcap_loop()
+    def test1_pcap_lib_version(self):
+        ver = pypcap.pcap_lib_version()
+        self.assertTrue( ver, 'Could not get libpcap version')
+
+    def test2_pcap_findalldevs(self):
+        devs = pypcap.pcap_findalldevs()
+        self.assertTrue( devs, 'Could not get devices')
+    
+    def test3_pcap_lookupdev(self):
+        devs = pypcap.pcap_findalldevs()
+        dev = pypcap.pcap_lookupdev()
+        self.assertTrue( dev in devs , 'Could not find device in list of available devices %s'%(','.join(devs)))
+
+    def test4_pcap_lookupnet(self):
+        devs = pypcap.pcap_findalldevs()
+        dev = pypcap.pcap_lookupdev()
+        details = None
+        for dev in devs:
+            try:
+                details = pypcap.pcap_lookupnet( dev )
+                break
+            except Exception,e:
+                pass
+        self.assertTrue( details, 'Could not lookup network details for any interface (%s)'%(','.join(devs)))
+    
+class ClassTests(unittest.TestCase):
+    def setUp(self):
+        self.pcap = pypcap.pcap()
+        devs = pypcap.pcap_findalldevs()
+        for dev in devs:
+            try:
+                pypcap.pcap_lookupnet( dev )
+                break
+            except Exception,e:
+                pass
+
+        print 'Found %s interface, and it has network'%( dev )
+        pcap = self.pcap.pcap_create(dev)
+        self.assertTrue( pcap, 'Error creating a pcap object')
+
+
+if __name__ == '__main__':
+    unittest.main()
+    exit()
